15254e14489ef4f36177256fdca1f216a2c43438
InfernalKn1ght <arzhaev.dmitry@yandex.ru>
2024-09-28 19:16:48 +0300

    [Task-1] Insertion sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
+use std::io;
 
+fn main() {
+	let mut nums = String::new();
+    io::stdin()
+		.read_line(&mut nums)
+		.expect("Failed to read line");
+
+	let nums = nums.trim().split_whitespace();
+	let mut v: Vec<i32> = Vec::new();	
+
+	for i in nums {
+		v.push(i.parse().expect("Not a number!"));
+	}
+
+	insertion_sort(&mut v);
+
+	for x in v {
+		print!("{x} ");
+	}
+}
+
+fn insertion_sort(v: &mut Vec<i32>) {
+	for i in 1..v.len() {
+		for j in (1..i+1).rev() {
+			if v[j] < v[j - 1] {
+				let temp = v[j] + v[j - 1];
+				v[j] = v[j - 1];
+				v[j - 1] = temp - v[j];
+			}
+		}
+	}
 }
